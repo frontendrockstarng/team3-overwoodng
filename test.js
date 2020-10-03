ceb3e9cff60857e4704db7a734bff2e5db070d52
frontendrockstarng <65969343+frontendrockstarng@users.noreply.github.com>
2020-10-03 20:32:47 +0100

    new code

---
+++ test.js
@@ -0,0 +1,28 @@
+
+
+
+$(document).ready(function(){
+    $('.cirlce-content').click(function(){
+     $('.main-tab').toggleClass('hidden');
+    });
+
+    $('.cirlce-content2').click(function(){
+        $('.main-tab2').toggleClass('hidden');
+       });
+
+       $('.cirlce-content3').click(function(){
+        $('.main-tab3').toggleClass('hidden');
+       });
+
+       $('.cirlce-content4').click(function(){
+        $('.main-tab4').toggleClass('hidden');
+       });
+
+       $('.cirlce-content5').click(function(){
+        $('.main-tab5').toggleClass('hidden');
+       });
+
+       $('.cirlce-content6').click(function(){
+        $('.main-tab6').toggleClass('hidden');
+       });
+});
